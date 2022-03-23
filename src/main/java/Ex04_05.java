83cbce0c7a6b1598a5ca26876f30140222e535af
KimSeongTaeg <dragonk1020@gmail.com>
2022-03-23 11:26:41 +0900

    java exam

---
+++ src/main/java/Ex04_05.java
@@ -0,0 +1,14 @@
+public class Ex04_05 {
+    public static void main(String[] args) {
+        int a = 100, b = 200;
+
+        System.out.printf("%d == %d 는 %s 이다 \n", a, b, a == b);
+        System.out.printf("%d != %d 는 %s 이다 \n", a, b, a != b);
+        System.out.printf("%d > %d 는 %s 이다 \n", a, b, a > b);
+        System.out.printf("%d < %d 는 %s 이다 \n", a, b, a < b);
+        System.out.printf("%d >= %d 는 %s 이다 \n", a, b, a >= b);
+        System.out.printf("%d <= %d 는 %s 이다 \n", a, b, a <= b);
+
+        System.out.printf("%d = %d 는 %s 이다 \n", a, b, a = b);
+    }
+}
