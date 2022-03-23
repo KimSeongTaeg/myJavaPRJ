de81aa5633d96e653dc109bd65d8a886bd27bdce
KimSeongTaeg <dragonk1020@gmail.com>
2022-03-23 10:46:36 +0900

    java exam

---
+++ src/main/java/Ex04_03.java
@@ -0,0 +1,27 @@
+public class Ex04_03 {
+    public static void main(String[] args) {
+        int a = 10;
+
+        a++;
+        System.out.printf("a ++ ==> %d \n", a);
+
+        a--;
+        System.out.printf("a -- ==> %d \n", a);
+
+        a += 5;
+        System.out.printf("a += 5 ==> %d \n", a);
+
+        a -= 5;
+        System.out.printf("a -= 5 ==> %d \n", a);
+
+        a *= 5;
+        System.out.printf("a *= 5 ==> %d \n", a);
+
+        a /= 5;
+        System.out.printf("a /= 5 ==> %d \n", a);
+
+        a %= 5;
+        System.out.printf("a %%= 5 ==> %d \n", a);
+
+    }
+}
