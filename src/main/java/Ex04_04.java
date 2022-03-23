a176bc3343d4dfa1f1d55c9a495e7faed3ada7f8
KimSeongTaeg <dragonk1020@gmail.com>
2022-03-23 11:15:23 +0900

    java exam

---
+++ src/main/java/Ex04_04.java
@@ -0,0 +1,11 @@
+public class Ex04_04 {
+    public static void main(String[] args) {
+        int a = 10, b;
+
+        b = a++;
+        System.out.printf("%d \n", b);
+
+        b = ++a;
+        System.out.printf("%d \n", b);
+    }
+}
