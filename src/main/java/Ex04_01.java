c4cb1ee83dd5820abafadb2bcf3120bbcf83d2f2
KimSeongTaeg <dragonk1020@gmail.com>
2022-03-23 10:08:20 +0900

    java exam

---
+++ src/main/java/Ex04_01.java
@@ -0,0 +1,21 @@
+public class Ex04_01 {
+    public static void main(String[] args) {
+        int a , b = 5 , c = 3;
+
+        a = b + c;
+        System.out.printf("%d + %d = %d \n",b,c,a);
+
+        a = b - c;
+        System.out.printf("%d - %d = %d \n",b,c,a);
+
+        a = b * c;
+        System.out.printf("%d * %d = %d \n",b,c,a);
+
+        a = b / c;
+        System.out.printf("%d / %d = %d \n",b,c,a);
+
+        a = b % c;
+        System.out.printf("%d %% %d = %d \n",b,c,a);
+
+    }
+}
