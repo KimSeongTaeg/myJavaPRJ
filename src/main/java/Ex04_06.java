cd48a87786c3269393a9368f5aefab436db32fed
KimSeongTaeg <dragonk1020@gmail.com>
2022-03-30 09:34:08 +0900

    java exam

---
+++ src/main/java/Ex04_06.java
@@ -0,0 +1,9 @@
+public class Ex04_06 {
+    public static void main(String[] args) {
+        int a = 99;
+
+        System.out.printf("AND 연산 : %s \n",(a >= 100) && (a <= 200));
+        System.out.printf("OR 연산 : %s \n",(a >= 100) || (a <= 200));
+        System.out.printf("NOT 연산 : %s \n",!(a == 100));
+    }
+}
