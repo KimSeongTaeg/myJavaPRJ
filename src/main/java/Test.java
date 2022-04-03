356d61bf9ad451cdd9e650c42036d081c782f27e
KimSeongTaeg <dragonk1020@gmail.com>
2022-04-04 08:56:41 +0900

    java exam

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,2 +1,45 @@
+import java.util.Scanner;
+
 public class Test {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a;
+
+        System.out.printf("점수를 입력하세요 : ");
+        a = s.nextInt();
+
+        if (a >= 95) {
+            System.out.printf("A");
+        } else {
+            if (a > 90) {
+                System.out.printf("A-");
+            } else {
+                if (a >= 85) {
+                    System.out.printf("B");
+                } else {
+                    if (a >= 80) {
+                        System.out.printf("B-");
+                    } else {
+                        if (a >= 75) {
+                            System.out.printf("c");
+                        } else {
+                            if (a >= 70) {
+                                System.out.printf("C-");
+                            } else {
+                                if (a >= 65) {
+                                    System.out.printf("D");
+                                } else {
+                                    if (a > 60) {
+                                        System.out.printf("D-");
+                                    } else {
+                                        System.out.printf("F");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
